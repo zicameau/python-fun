1482eea3c8193c9898a10be4af2958fda4184440
circa10a <caleblemoine@gmail.comS>
2017-10-02 16:51:22 -0500

    add setup.py

---
+++ build/scripts-2.7/http_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import requests
+def check(url):
+    try:
+        r = requests.get(url)
+        return(r.status_code)
+
+    except requests.ConnectionError:
+        print('failed to connect')
+
+    except:
+        print('unknown error')
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+name='http_code',
+version='0.1',
+scripts=['http_code.py']
+)
