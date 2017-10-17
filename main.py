ed572c1f8aa9b69dfbdcf92bd760909c38b68bf7
circa10a <caleblemoine@gmail.comS>
2017-10-17 18:06:52 -0500

    add random rainbow checkmark, because.. why not

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 import sys
+from termcolor import colored
+from random import randint
+#local modules in ./lib
 import lib.http_code as http_code
 import lib.validate as validate
-from termcolor import colored
+
+
 
 def main():
+    colors = ['red','yellow','green','blue','magenta','cyan']
+    # -1 to get accurate count of array elements (0,5)
+    num_colors = int(len(colors)) - 1
+    #pick random number within length of array
+    rand_color = colors[randint(0, num_colors)]
+
     #Validate argument is passed to script
     try:
         site = sys.argv[1]
     except:
         print colored('no site argument passed e.g. https://google.com', 'red')
-        exit(1)
+        sys.exit(1)
+
     #Validate argument starts with http or https from valid_url module
     validate.url(site)
 
@@ -18,8 +29,10 @@ def main():
     response = http_code.check(site)
 
     #print response code and color output depending if successful or not
-    if (response == 200):
-        print colored(response, 'green')
+    if response == 200:
+        print colored(response, 'green'),
+        # Print random color check mark for kicks
+        print colored(u'\u2713', rand_color)
     else:
         print colored(response, 'red')
 
