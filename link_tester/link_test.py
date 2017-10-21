0f2c2e2b11b4b3ba7acf7f0ae48e15ef5ed61343
circa10a <caleblemoine@gmail.comS>
2017-10-21 16:05:09 -0500

    restructure, add link tester

---
+++ link_tester/link_test.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests,sys
+from termcolor import colored
+
+def main():
+    try:
+        site = sys.argv[1]
+    except:
+        print colored('no site argument passed e.g. https://google.com', 'red')
+        sys.exit(1)
+
+    #Validate argument starts with http or https
+    if site[:7] == 'http://':
+        pass
+    elif site[:8] == 'https://':
+        pass
+    else:
+        print colored('invalid URL passed as arg.', 'red')
+        exit(1)
+
+    r  = requests.get(site)
+    data = r.text
+    soup = BeautifulSoup(data, 'html.parser')
+
+    for link in soup.find_all('a'):
+        usable_link=str(link.get('href'))
+        
+        if usable_link[:7] == 'http://':
+            print (usable_link)
+            response=(str(requests.get(usable_link)).split()[1].replace('>',''))
+
+            if response == '[200]':
+                print colored(response, 'green'),
+            else:
+                print colored(response, 'red')
+
+        elif usable_link[:8] == 'https://':
+            print(usable_link)
+            response=(str(requests.get(usable_link)).split()[1].replace('>',''))
+
+            if response == '[200]':
+                print colored(response, 'green'),
+            else:
+                print colored(response, 'red')
+
+        else:
+            pass
+
+if __name__ == "__main__":
+    main()
