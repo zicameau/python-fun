796e6ec90d6210cca6397b41f79912a7fd7d4dc5
circa10a <caleblemoine@gmail.comS>
2017-10-22 09:39:37 -0500

    add requirements.txt

--- link_tester/link_test.py
+++ link_tester/link_test.py
@@ -5,13 +5,11 @@ import requests, sys
 from termcolor import colored
 
 def check_protocol(site): #Validate argument starts with http or https
-    return bool(__import__('re').search('^http(|s):\/\/', site))
+    return site.startswith('http://') or site.startswith('https://')
 
-def check_response(response, link):
-    if response == '[200]':
-        print colored(response, 'green'), link
-    else:
-        print colored(response, 'red'), link
+def print_response(response, link):
+    color = 'green' if response == 200 else 'red'
+    print colored('[' + str(response) + ']', color), link
 
 def test_links(site):
     try:
@@ -19,21 +17,18 @@ def test_links(site):
     except requests.ConnectionError:
         print colored('Unable to Connect' , 'red')
         sys.exit(1)
-    data = r.text
-    soup = BeautifulSoup(data, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
     for link in soup.find_all('a'):
         link=str(link.get('href'))
         if check_protocol(link):
-            response=(str(requests.get(link)).split()[1].replace('>',''))
-            check_response(response, link)
-        else:
-            pass
+            response=requests.get(link)
+            print_response(response.status_code, link)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         site = str(sys.argv[1])
         if not check_protocol(site):
-            print colored('invalid URL passed as arg.', 'red')
+            print colored('invalid URL passed as arg. (Does not start with http/https)', 'red')
             sys.exit(1)
         try:
             test_links(site)
