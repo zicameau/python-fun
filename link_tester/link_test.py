78808fe8b8c764fe2a2f7ebbf2a44dd77b2ca565
Trevor <rowdeil@gmail.com>
2017-11-02 21:40:19 -0700

    Changing print statements in link_test.py
    Making link_test.py python3 compatible by changing the print statements.

--- link_tester/link_test.py
+++ link_tester/link_test.py
@@ -8,13 +8,13 @@ def check_protocol(site): #Validate argument starts with http or https
 
 def print_response(response, link):
     color = 'green' if response == 200 else 'red'
-    print colored('[' + str(response) + ']', color), link
+    print (colored('[' + str(response) + ']', color), link)
 
 def test_links(site):
     try:
         r  = requests.get(site)
     except requests.ConnectionError:
-        print colored('Unable to Connect' , 'red')
+        print (colored('Unable to Connect' , 'red'))
         sys.exit(1)
     soup = BeautifulSoup(r.text, 'html.parser')
     for link in soup.find_all('a'):
@@ -27,12 +27,12 @@ if __name__ == '__main__':
     try:
         site = str(sys.argv[1])
         if not check_protocol(site):
-            print colored('invalid URL passed as arg. (Does not start with http/https)', 'red')
+            print (colored('invalid URL passed as arg. (Does not start with http/https)', 'red'))
             sys.exit(1)
         try:
             test_links(site)
         except KeyboardInterrupt:
-            print colored('Interrupted, bye.', 'red')
+            print (colored('Interrupted, bye.', 'red'))
     except IndexError:
-        print colored('no site argument passed e.g. https://google.com', 'red')
+        print (colored('no site argument passed e.g. https://google.com', 'red'))
         sys.exit(1)
